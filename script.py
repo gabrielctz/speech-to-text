c297504a8af78bc1764e908cef0e67618f7dc93d
GabrielCtrz <133511026+gabrielctz@users.noreply.github.com>
2023-09-28 20:16:11 +0000

    script.py

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ from easygui import fileopenbox
 import sys
 
 init()
-
+os.system("title Speech To Text | Developer : GabrielCtz")
 def package():
     folder = f"C:\\Users\\{getpass.getuser()}\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\speech_recognition\\pocketsphinx-data\\fr-FR"
     folders = f"C:\\Users\\{getpass.getuser()}\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\speech_recognition\\pocketsphinx-data"
@@ -31,20 +31,15 @@ def package():
 
 def main():
     os.system("cls")
-    package()
     print(f"\n ╔\n ║ {Fore.GREEN} Speech to text {Fore.RED} 2k23{Fore.RESET}\n ║ {Fore.GREEN} Developer: {Fore.RED} GabrielCtz{Fore.RESET}\n ╚")
     files = fileopenbox(msg="Sélectionnez votre audio à convertir.")
 
     if files.endswith(".wav"):
-        try:
             r = sr.Recognizer()
             with sr.AudioFile(files) as source:
                 audio = r.record(source)
                 text = r.recognize_sphinx(audio, language="fr-FR")
-                print(text)
-        except:
-            print(f'\n [{Fore.RED}!{Fore.RESET}] La conversation de "speech to text" a échoué ...')
-            main()
+                print(f"\n [ {text} ]")
     else:
         main()
 
